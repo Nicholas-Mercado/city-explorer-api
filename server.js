f5f44d73c1122f966d69a954350ef0f58cff9bb3
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-22 18:04:43 -0800

    clean up code

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ console.log('proof of life');
 const express = require('express');
 
 const app = express();
-
 require('dotenv').config();
 let data = require('./data/weather.json');
 
@@ -17,25 +16,23 @@ const PORT = process.env.PORT || 3002;
 
 app.get('/weather', (request, response) =>{
   let searchQuery = request.query.searchQuery;
-  console.log(searchQuery);
+  // console.log(searchQuery);
   
   let  cityObj = data.find(city => city.city_name === searchQuery);
-  let selectedCity = new Forecast(cityObj);
-  console.log(cityObj);
-  // response.send(cityObj);
+
   const weatherArr = cityObj.data.map(day => new Forecast(day));
   console.log(cityObj);
   response.send(weatherArr);
 })
 
+class Forecast {
+  constructor(day){
+    this.date = day.datetime,
+    this.description = day.weather.description
+  }
+}
 app.get('*',(request, response) =>{
   response.send('ERROR')
 })
 
-class Forecast {
-  constructor(day){
-    this.description = day.weather;
-    this.date = day.datetime
-  }
-}
 app.listen(PORT,() => console.log(`listen test on port ${PORT}`))
