81804f70f856694fe0ae4fb7eb2bb6b7581b579d
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-22 22:21:40 -0800

    commented out app.use

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.get('*',(request, response) =>{
   response.send('ERROR')
 })
 
-app.use((error, request, response, next) =>{
-  response.status(500).send(error.message)
-})
+// app.use((error, request, response, next) =>{
+//   response.status(500).send(error.message)
+// })
 
 app.listen(PORT,() => console.log(`listen test on port ${PORT}`))
