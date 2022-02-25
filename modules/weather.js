8371d715d780f198249258295d2401776d958259
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-25 15:20:19 -0800

    Working server with weather cashe

---
+++ modules/weather.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let cache = require('./cache.js');
+const axios = require('axios');
+// added async
+async function getWeather(latitude, longitude) {
+  const key = 'weather-' + latitude + latitude;
+
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&days=5&lat=${latitude}&lon=${longitude}`;
+  
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = axios.get(url)
+    .then(response => parseWeather(response.data));
+  }
+  
+  console.log(cache[key].data);
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Weather(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+class Weather {
+  constructor(day) {
+    this.date = day.datetime;
+    this.description = day.weather.description;
+  }
+}
+// moved to bottom
+module.exports = getWeather;
--- server.js
+++ server.js
@@ -1,51 +1,32 @@
 'use strict';
 
-console.log('I am Alive!!');
+console.log('I am Alive!! yes this is the new server');
 
 require('dotenv').config();
 const express = require('express');
 const app = express();
-let data = require('./data/weather.json');
+const weather = require('./modules/weather.js');
 const cors = require('cors');
 const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 
 // import modules
-const getWeather = require('./weather.js');
+app.get('/weather', weatherHandler);
+
+//hot fix to get movies working while I debug the new weather*******
 const getMovies = require('./movies.js');
-
-app.get('/weather', getWeather);
 app.get('/movies', getMovies);
+// ********************************************
 
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+  .then(summaries => response.send(summaries))
+  .catch((error) => {
+    console.error(error);
+    response.status(200).send('Sorry. Something went wrong!')
+  });
+}  
 
-
-
-
-
-
-app.get('*',(request, response) =>{
-  response.send('ERROR')
-})
-
-app.use((error, request, response, next) =>{
-  response.status(500).send(error.message)
-})
-
-class Forecast {
-  constructor(day){
-    this.date = day.datetime;
-    this.description = day.weather.description;
-    this.low = day.low_temp;
-    this.high = day.high_temp
-
-  }
-}
-
-
-
-app.listen(PORT,() => console.log(`listen test on port ${PORT}`))
-
-
-
-
+app.listen(PORT, () => console.log(`Server up on ${PORT}`));
